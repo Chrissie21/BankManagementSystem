f417a5c09b4704c95cd4f9ca3b481fed08ff3b81
Chrissie21 <chrisbalele21@gmail.com>
2025-01-03 08:35:04 +0300

    Added transaction log and log transaction to Account.h

--- src/Account.h
+++ src/Account.h
@@ -2,18 +2,23 @@
 #define ACCOUNT_H
 
 #include <string>
+#include <vector>
 using namespace std;
 
 class Account {
 protected:
     string accountNumber;
     double balance;
+    vector<string> transactionLog; // Add transaction log
+
+    void logTransaction(const string& entry); // Log transactions
 
 public:
     Account(string acctNum, double bal);
     virtual void display() const;
     virtual void deposit(double amount);
     virtual void withdraw(double amount);
+    void displayTransactions() const; // Display transaction log
     virtual ~Account();
 };
 
