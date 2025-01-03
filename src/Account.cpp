3dc02d6401b0af92ca62211b94822e0306fec671
Chrissie21 <chrisbalele21@gmail.com>
2025-01-03 03:52:29 +0300

    Implemented Constructor,Deposit,Withdraw,Display and destructor methods

--- src/Account.cpp
+++ src/Account.cpp
@@ -1,35 +1,30 @@
 #include "Account.h"
+#include <iostream>
+using namespace std;
 
-// Constructor
 Account::Account(string acctNum, double bal) : accountNumber(acctNum), balance(bal) {}
 
-// Deposit method
-void Account::deposit(double amount) {
-    if(amount > 0) {
-        balance += amount;
-        cout << "Deposited " << amount << " into account " << accountNumber << ". New balance: " << balance << endl;
-    } else {
-        cout << "Invalid deposit amount." << endl;
-    }
-}
-
-// Withdraw method
-void Account::withdraw(double amount) {
-    if(amount > 0 && amount <= balance) {
-        balance -= amount;
-        cout << "Withdrew " << amount << " from account " << accountNumber << ". New balance: " << balance << endl;
-    } else {
-        cout << "Invalid withdrawal amount." << endl;
-    }
-}
-
-// Get balance method
-double Account::getBalance() {
-    return balance;
-}
-
-// Display method
-void Account::display() {
+void Account::display() const {
     cout << "Account Number: " << accountNumber << endl;
     cout << "Balance: " << balance << endl;
 }
+
+void Account::deposit(double amount) {
+    if(amount > 0) {
+        balance += amount;
+        cout << "Deposited " << amount << ". New balance: " << balance << endl;
+    } else {
+        cout << "Deposit amount must be positive!" << endl;
+    }
+}
+
+void Account::withdraw(double amount) {
+    if(amount > 0 && amount <= balance) {
+        balance -= amount;
+        cout << "Withdrew " << amount << ". New balance: " << balance << endl;
+    } else {
+        cout << "Insufficient funds or invalid withdrawal amount!" << endl;
+    }
+}
+
+Account::~Account() {}
